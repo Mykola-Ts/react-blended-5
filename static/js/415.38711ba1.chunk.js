8558d038ad79bd73d16d3eda0f8e531d14de3557
Mykola-Ts <125122747+Mykola-Ts@users.noreply.github.com>
2023-10-22 14:01:53 +0000

    Deploying to gh-pages from @ Mykola-Ts/react-blended-5@f4fa486c1da11f0d2eb883655e410f2aa1788dc9 🚀

--- static/js/415.38711ba1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_blended_5=self.webpackChunkreact_blended_5||[]).push([[415],{415:function(e,n,r){r.r(n);var t=r(184);n.default=function(){return(0,t.jsx)(t.Fragment,{children:"Home"})}}}]);
-//# sourceMappingURL=415.38711ba1.chunk.js.map
